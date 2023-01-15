31edf121188113bf9ebac68dc2c4e7069ff35681
kpn <kpn@cae.com>
2023-01-15 15:42:35 -0500

    Initial Commit

---
+++ Guidomia-Ken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia-Ken
+//
+//  Created by Kenworth Nadera on 2023-01-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
