d4cb36ba3b0cb4ce637e5e2b589034c08b214a9a
kpn <kpn@cae.com>
2023-01-16 10:24:07 -0500

    added logic for pros/cons

--- Guidomia-Ken/CarsModule/Views/CarCell.swift
+++ Guidomia-Ken/CarsModule/Views/CarCell.swift
@@ -18,7 +18,11 @@ class CarCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
+        imageThumb.image = UIImage()
+        labelTitle.text = ""
+        labelPrice.text = ""
+        labelRating.text = ""
+        labelProsCons.text = ""
     }
     
     func configure(car: Car) {
@@ -26,7 +30,23 @@ class CarCell: UITableViewCell {
         labelTitle.text = "\(car.make) \(car.model)"
         labelPrice.text = "Price: \(Int((car.marketPrice/1000)))k"
         labelRating.text = (1...car.rating).map({ _ in return "★" }).joined(separator:" ")
-        labelProsCons.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        labelProsCons.attributedText = generateProsConsAttributedString(for: car.prosList, consList: car.consList)
+    }
+    
+    private func generateProsConsAttributedString(for prosList: [String], consList: [String]) -> NSAttributedString {
+        var prosText = NSAttributedString()
+        let pros = prosList.filter({ !$0.isEmpty })
+        if (pros.count > 0) {
+            prosText = NSAttributedString().sectionText(for: "Pros:", inputArray: pros)
+        }
+        
+        var consText = NSAttributedString()
+        let cons = consList.filter({ !$0.isEmpty })
+        if (cons.count > 0) {
+            consText = NSAttributedString().sectionText(for: "Cons:", inputArray: cons)
+        }
+        
+        return NSAttributedString().combine(attributedStrings: [prosText, consText])
     }
 }
 
@@ -39,3 +59,53 @@ extension UITableViewCell {
     self.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 5, right: 15)
   }
 }
+
+
+extension NSAttributedString {
+    func sectionText(for title:String, inputArray:[String]) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString()
+        
+        // add title
+        attributedString.append(sectionTitle(with: title))
+        attributedString.append(NSAttributedString(string: "\n"))
+        
+        // add each item a bullet and description
+        inputArray.forEach {
+            attributedString.append(sectionBullet(with: "•"))
+            attributedString.append(sectionItems(with: "\($0)"))
+            attributedString.append(NSAttributedString(string: "\n"))
+        }
+        
+        return attributedString
+    }
+    
+    func combine(attributedStrings: [NSAttributedString]) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString()
+        for str in attributedStrings {
+            attributedString.append(str)
+        }
+        return attributedString
+    }
+    
+    private func sectionTitle(with title: String) -> NSAttributedString {
+        var attributes = [NSAttributedString.Key: AnyObject]()
+        attributes[.foregroundColor] = UIColor.systemGray
+        attributes[.font] =  UIFont.systemFont(ofSize: 20, weight: .bold)
+        return NSAttributedString(string: title, attributes: attributes)
+    }
+    
+    private func sectionBullet(with bullet: String) -> NSAttributedString {
+        var attributes = [NSAttributedString.Key: AnyObject]()
+        attributes[.foregroundColor] = UIColor.orange
+        attributes[.font] = UIFont.systemFont(ofSize: 22, weight: .bold)
+        return NSAttributedString(string: "\(bullet) ", attributes: attributes)
+    }
+    
+    private func sectionItems(with description: String) -> NSAttributedString {
+        var attributes = [NSAttributedString.Key: AnyObject]()
+        attributes[.foregroundColor] = UIColor.black
+        return NSAttributedString(string: description, attributes: attributes)
+    }
+}
+
+
