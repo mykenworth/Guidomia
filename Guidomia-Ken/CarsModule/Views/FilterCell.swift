3f37eacba97bd99157918bce64a1503b3e1f911e
kpn <kpn@cae.com>
2023-01-16 15:04:15 -0500

    added filter UI and logic

---
+++ Guidomia-Ken/CarsModule/Views/FilterCell.swift
@@ -0,0 +1,48 @@
+//
+//  FilterCell.swift
+//  Guidomia-Ken
+//
+//  Created by Kenworth Nadera on 2023-01-16.
+//
+
+import UIKit
+
+struct FilterItem {
+    let text: String
+    let type: FilterType
+}
+
+enum FilterType {
+    case model
+    case make
+}
+
+protocol FilterCellDelegate: AnyObject {
+    func didSearch(filter: FilterItem)
+}
+
+class FilterCell: UITableViewCell {
+    
+    @IBOutlet weak var textfieldMake: DropDown!
+    @IBOutlet weak var textfieldModel: DropDown!
+    
+    weak var delegate: FilterCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureFilter(with makes: [FilterItem], models: [FilterItem]) {
+        textfieldMake.optionArray = makes.map({ $0.text })
+        textfieldMake.didSelect { selectedText, index, id in
+            self.delegate?.didSearch(filter: FilterItem(text: selectedText, type: .make))
+        }
+        
+        textfieldModel.optionArray = models.map({ $0.text })
+        textfieldModel.didSelect { selectedText, index, id in
+            self.delegate?.didSearch(filter: FilterItem(text: selectedText, type: .model))
+        }
+    }
+}
+
